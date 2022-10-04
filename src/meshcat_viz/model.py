10eb524c6b2db906a03e973a404d96b2a6c3162f
Diego Ferigo <diego.ferigo@iit.it>
2022-10-04 14:04:04 +0200

    Initialize Python package

---
+++ src/meshcat_viz/model.py
@@ -0,0 +1,139 @@
+import dataclasses
+import itertools
+from collections import defaultdict
+from typing import DefaultDict, Dict, List, Optional, Sequence
+
+import meshcat
+import numpy as np
+import numpy.typing as npt
+from jaxsim import logging
+from meshcat.visualizer import Visualizer
+
+
+@dataclasses.dataclass
+class MeshcatModel:
+
+    name: str
+    visualizer: Visualizer = dataclasses.field(repr=False)
+
+    NodeName = str
+    LinkName = str
+
+    # Dictionary from the link name defined in the SDF/URDF to the scoped named
+    # used in the MeshCat visualizer
+    link_to_node: Dict[LinkName, NodeName] = dataclasses.field(default_factory=dict)
+
+    # Each link could have multiple visual shapes attached, each of them rigidly
+    # attached to the link through a constant transforms.
+    # We build the nodes tree in such a way that all visual shapes are automatically
+    # moved when their corresponding link is moved.
+    visual_shapes: DefaultDict[LinkName, List[NodeName]] = dataclasses.field(
+        default_factory=defaultdict
+    )
+
+    def __post_init__(self):
+
+        # Initialize the root node of the model
+        self.visualizer[self.name].set_object(meshcat.geometry.Box([0.0, 0.0, 0.0]))
+
+        if self.visual_shapes.default_factory not in {None, list}:
+            logging.warning("Changing default factory of shapes dict to 'list'")
+
+        if self.visual_shapes.default_factory is None:
+            self.visual_shapes.default_factory = list
+
+    def nodes_names(self, include_visual_nodes: bool = False) -> List[str]:
+
+        model_nodes = [self.name] + list(self.link_to_node.values())
+
+        if not include_visual_nodes:
+            return model_nodes
+
+        all_visual_shapes_nodes = list(
+            itertools.chain.from_iterable(self.visual_shapes.values())
+        )
+
+        return model_nodes + all_visual_shapes_nodes
+
+    def delete(self) -> None:
+
+        self.visualizer[self.name].delete()
+
+    def set_base_pose(
+        self,
+        transform: Optional[npt.NDArray] = None,
+        position: Optional[npt.NDArray] = None,
+        quaternion: Optional[npt.NDArray] = None,
+    ) -> None:
+
+        self.set_node_pose(
+            node_name=self.name,
+            transform=transform,
+            position=position,
+            quaternion=quaternion,
+        )
+
+    def set_link_pose(
+        self,
+        link_name: str,
+        transform: Optional[npt.NDArray] = None,
+        position: Optional[npt.NDArray] = None,
+        quaternion: Optional[npt.NDArray] = None,
+    ) -> None:
+
+        if link_name not in self.link_to_node:
+            raise ValueError(link_name)
+
+        self.set_node_pose(
+            node_name=self.link_to_node[link_name],
+            transform=transform,
+            position=position,
+            quaternion=quaternion,
+        )
+
+    def set_node_pose(
+        self,
+        node_name: str,
+        transform: Optional[npt.NDArray] = None,
+        position: Optional[npt.NDArray] = None,
+        quaternion: Optional[npt.NDArray] = None,
+    ) -> None:
+
+        if node_name not in self.nodes_names():
+            raise ValueError(f"Failed to find node '{node_name}'")
+
+        if {type(transform), type(position), type(quaternion)} == {None}:
+            raise ValueError
+
+        if transform is None and {type(position), type(quaternion)} == {None}:
+            raise ValueError
+
+        if position is None and quaternion is not None:
+            position = np.zeros(4)
+
+        if quaternion is None and position is not None:
+            quaternion = np.array([1.0, 0, 0, 0])
+
+        # Note: the transform of the node is always wrt its parent.
+        # We build the graph so that the visual shapes are rigidly attached to their
+        # parent link (therefore updating the link pose would move all its visuals),
+        # and all links are children of the model's root (so that changing the root pose
+        # moves the entire model).
+        # This means that the link poses must be defined wrt the model's root.
+        # The only exceptions are links considered as frames, in this case they are
+        # rigidly attached to their parent link and their pose is constant.
+
+        if transform is not None:
+            parent_H_node = np.array(transform, dtype=float)
+
+        elif position is not None and quaternion is not None:
+            parent_H_node = meshcat.transformations.quaternion_matrix(
+                quaternion=quaternion
+            )
+            parent_H_node[0:3, 3] = position
+
+        else:
+            raise RuntimeError
+
+        # Update the node pose
+        self.visualizer[node_name].set_transform(parent_H_node)
---
+++ src/meshcat_viz/world.py
@@ -0,0 +1,173 @@
+import pathlib
+from typing import Dict, List, Optional, Sequence, Tuple, Union
+
+import jax
+import jaxsim.high_level.model
+import numpy as np
+import rod
+from meshcat.servers.zmqserver import start_zmq_server_as_subprocess
+from meshcat.visualizer import Visualizer
+
+from .model import MeshcatModel
+from .model_builder import MeshcatModelBuilder
+
+
+class MeshcatWorld:
+    def __init__(self, dt: float = 0.001, rtf: float = 1.0):
+
+        self.dt = dt
+        self.rtf = rtf
+
+        self._visualizer = None
+
+        self._fk = jax.jit(lambda m: m.forward_kinematics())
+        self._meshcat_models: Dict[str, MeshcatModel] = dict()
+        self._jaxsim_models: Dict[str, jaxsim.high_level.model.Model] = dict()
+
+    def open(self) -> None:
+
+        _ = self.meshcat_visualizer
+
+    def close(self) -> None:
+
+        if self._visualizer is not None:
+
+            self.meshcat_visualizer.delete()
+
+            self._jaxsim_models = dict()
+            self._meshcat_models = dict()
+            self._visualizer = None
+
+    def update_model(
+        self,
+        model_name: str,
+        joint_positions: Optional[Sequence] = None,
+        joint_names: Optional[List[str]] = None,
+        base_position: Optional[Sequence] = None,
+        base_quaternion: Optional[Sequence] = None,
+    ) -> None:
+
+        if model_name not in self._meshcat_models:
+            raise ValueError(model_name)
+
+        if model_name not in self._jaxsim_models:
+            raise ValueError(model_name)
+
+        if base_position is not None:
+            self._jaxsim_models[model_name].reset_base_position(
+                position=np.array(base_position)
+            )
+
+        if base_quaternion is not None:
+            self._jaxsim_models[model_name].reset_base_orientation(
+                orientation=np.array(base_quaternion)
+            )
+
+        # Update transform of base link's node
+        W_H_B = self._jaxsim_models[model_name].base_transform()
+        self._meshcat_models[model_name].set_base_pose(transform=W_H_B)
+
+        # TODO: use whole-body FK
+        if joint_positions is not None:
+
+            # Store the new joint configuration
+            self._jaxsim_models[model_name].reset_joint_positions(
+                positions=np.atleast_1d(joint_positions), joint_names=joint_names
+            )
+
+            # Compute forward kinematics with JaxSim
+            W_H_i = self._fk(self._jaxsim_models[model_name])
+
+            # In MeshCat, all link poses are relative to the model's base
+            B_H_W = np.linalg.inv(W_H_B)
+            B_H_i = B_H_W @ W_H_i
+
+            # Send to the visualizer the updated transforms
+            _ = [
+                self._meshcat_models[model_name].set_link_pose(
+                    link_name=l.name(),
+                    transform=np.array(B_H_i[l.index()], dtype=float),
+                )
+                for l in self._jaxsim_models[model_name].links()
+            ]
+
+    def insert_model(
+        self,
+        sdf: Union[str, pathlib.Path],
+        model_name: str = None,
+        model_pose: Optional[Tuple[Sequence, Sequence]] = None,
+    ) -> str:
+
+        # Create the ROD model from the SDF resource
+        rod_model = rod.Sdf.load(sdf=sdf).model
+
+        # Extract the model name if not given
+        if model_name is None and rod_model.name not in {None, ""}:
+            model_name = rod_model.name
+        else:
+            raise ValueError("Failed to assign a name to the model")
+
+        if model_name in self._meshcat_models.keys():
+            raise ValueError(f"Model '{model_name}' is already part of the world")
+
+        # Create the JaxSim model from the SDF resource
+        jaxsim_model = jaxsim.high_level.model.Model.build_from_sdf(
+            sdf=sdf, model_name=model_name
+        ).mutable(validate=True)
+
+        # Create the MeshcatModel
+        meshcat_model = MeshcatModelBuilder.from_kinematic_graph(
+            visualizer=self.meshcat_visualizer,
+            kinematic_graph=jaxsim_model.physics_model.description,
+            rod_model=rod_model,
+            model_name=model_name,
+        )
+
+        # Set the initial model pose
+        if model_pose is not None:
+
+            meshcat_model.set_base_pose(
+                position=np.array(model_pose[0]), quaternion=np.array(model_pose[1])
+            )
+
+        # Store the model
+        self._meshcat_models[meshcat_model.name] = meshcat_model
+
+        # Store the JaxSim model, used to compute forward kinematics
+        self._jaxsim_models[meshcat_model.name] = jaxsim_model
+
+        return meshcat_model.name
+
+    def remove_model(self, model_name: str) -> None:
+
+        if self._visualizer is None:
+            msg = "The Meshcat visualizer hasn't been opened yet, the are no models"
+            raise RuntimeError(msg)
+
+        if model_name not in self._meshcat_models.keys():
+            raise ValueError(f"Model '{model_name}' is not part of the visualization")
+
+        self._meshcat_models[model_name].delete()
+        self._meshcat_models.pop(model_name)
+
+    @property
+    def meshcat_visualizer(self) -> Visualizer:
+
+        if self._visualizer is not None:
+            return self._visualizer
+
+        # Start MeshCat server
+        server_proc, zmq_url, web_url = start_zmq_server_as_subprocess()
+
+        # Attach visualizer to server
+        meshcat_visualizer = Visualizer(zmq_url=zmq_url)
+        meshcat_visualizer.window.server_proc = server_proc
+
+        # Configure the visualizer
+        meshcat_visualizer["/Grid"].set_property("visible", True)
+        meshcat_visualizer["/Background"].set_property("visible", True)
+        meshcat_visualizer["/Background"].set_property("top_color", [1, 1, 1])
+        meshcat_visualizer["/Background"].set_property("bottom_color", [0, 0, 0])
+
+        self._visualizer = meshcat_visualizer
+        return self._visualizer
