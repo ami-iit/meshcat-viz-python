503213a3d989b7497f79df2472cf1e42a81abbfb
diegoferigo <diego.ferigo@iit.it>
2023-02-09 11:29:16 +0100

    Use node_path instead of node_name

--- src/meshcat_viz/model.py
+++ src/meshcat_viz/model.py
@@ -16,23 +16,23 @@ class MeshcatModel:
     name: str
     visualizer: MeshcatVisualizer = dataclasses.field(repr=False)
 
-    NodeName = str
+    NodePath = str
     LinkName = str
     FrameName = str
 
     # Dictionary from the link name defined in the SDF/URDF to the scoped named
     # used in the MeshCat visualizer
-    link_to_node: Dict[LinkName, NodeName] = dataclasses.field(default_factory=dict)
+    link_to_node: Dict[LinkName, NodePath] = dataclasses.field(default_factory=dict)
 
     # Dictionary from the frame name defined in the SDF/URDF to the scoped named
     # used in the MeshCat visualizer
-    frame_to_node: Dict[FrameName, NodeName] = dataclasses.field(default_factory=dict)
+    frame_to_node: Dict[FrameName, NodePath] = dataclasses.field(default_factory=dict)
 
     # Each link could have multiple visual shapes attached, each of them rigidly
     # attached to the link through a constant transforms.
     # We build the nodes tree in such a way that all visual shapes are automatically
     # moved when their corresponding link is moved.
-    visual_shapes: DefaultDict[LinkName, List[NodeName]] = dataclasses.field(
+    visual_shapes: DefaultDict[LinkName, List[NodePath]] = dataclasses.field(
         default_factory=defaultdict
     )
 
@@ -46,20 +46,35 @@ class MeshcatModel:
         if self.visual_shapes.default_factory is None:
             self.visual_shapes.default_factory = list
 
-    def nodes_names(
+    def nodes_paths(
         self, include_frame_nodes: bool = False, include_visual_nodes: bool = False
     ) -> List[str]:
-        model_nodes = [self.name] + list(self.link_to_node.values())
+        # Initialize the paths with the link nodes
+        model_node_paths = [self.name] + list(self.link_to_node.values())
 
         if include_frame_nodes:
-            model_nodes += list(self.frame_to_node.values())
+            model_node_paths += list(self.frame_to_node.values())
 
         if include_visual_nodes:
-            model_nodes += list(
+            # Note: we use itertools because visual_shapes is a List[NodePath]
+            model_node_paths += list(
                 itertools.chain.from_iterable(self.visual_shapes.values())
             )
 
-        return model_nodes
+        return model_node_paths
+
+    def get_node_path(self, node_name: str) -> str:
+        if node_name == self.name:
+            return self.name
+
+        elif node_name in self.link_to_node:
+            return self.link_to_node[node_name]
+
+        elif node_name in self.frame_to_node:
+            return self.frame_to_node[node_name]
+
+        else:
+            raise ValueError(f"Failed to find handled node '{node_name}'")
 
     def delete(self) -> None:
         self.visualizer[self.name].delete()
@@ -71,7 +86,7 @@ class MeshcatModel:
         quaternion: Optional[npt.NDArray] = None,
     ) -> None:
         self.set_node_pose(
-            node_name=self.name,
+            node_path=self.name,
             transform=transform,
             position=position,
             quaternion=quaternion,
@@ -88,7 +103,7 @@ class MeshcatModel:
             raise ValueError(link_name)
 
         self.set_node_pose(
-            node_name=self.link_to_node[link_name],
+            node_path=self.link_to_node[link_name],
             transform=transform,
             position=position,
             quaternion=quaternion,
@@ -96,15 +111,15 @@ class MeshcatModel:
 
     def set_node_pose(
         self,
-        node_name: str,
+        node_path: str,
         transform: Optional[npt.NDArray] = None,
         position: Optional[npt.NDArray] = None,
         quaternion: Optional[npt.NDArray] = None,
     ) -> None:
-        if node_name not in self.nodes_names(
+        if node_path not in self.nodes_paths(
             include_frame_nodes=True, include_visual_nodes=True
         ):
-            raise ValueError(f"Failed to find node '{node_name}'")
+            raise ValueError(f"Failed to find node '{node_path}'")
 
         if {type(transform), type(position), type(quaternion)} == {None}:
             raise ValueError
@@ -140,10 +155,10 @@ class MeshcatModel:
             raise RuntimeError
 
         # Update the node pose
-        self.visualizer[node_name].set_transform(parent_H_node)
+        self.visualizer[node_path].set_transform(parent_H_node)
 
     def set_link_transforms(
         self, link_names: Sequence[str], transforms: Sequence[npt.NDArray]
     ) -> None:
-        node_names = [self.link_to_node[l] for l in link_names]
-        self.visualizer.set_transforms(paths=node_names, matrices=transforms)
+        node_paths = [self.get_node_path(node_name=l) for l in link_names]
+        self.visualizer.set_transforms(paths=node_paths, matrices=transforms)
