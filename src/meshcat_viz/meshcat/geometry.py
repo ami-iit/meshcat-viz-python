a2f6d295a1ceb0afeccd31025d99dc2f96ed2cf1
diegoferigo <diego.ferigo@iit.it>
2023-07-18 17:26:11 +0200

    Add PlaneGeometry

--- src/meshcat_viz/meshcat/geometry.py
+++ src/meshcat_viz/meshcat/geometry.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from meshcat.geometry import Geometry
 
 
@@ -13,4 +15,29 @@ class Cone(Geometry):
             "type": "ConeGeometry",
             "radius": self.radius,
             "height": self.height,
+        }
+
+
+class PlaneGeometry(Geometry):
+    def __init__(
+        self,
+        width: float,
+        height: float,
+        widthSegments: Optional[int] = None,
+        heightSegments: Optional[int] = None,
+    ):
+        super(PlaneGeometry, self).__init__()
+        self.width = width
+        self.height = height
+        self.widthSegments = widthSegments if widthSegments is not None else width
+        self.heightSegments = heightSegments if heightSegments is not None else height
+
+    def lower(self, object_data):
+        return {
+            "uuid": self.uuid,
+            "type": "PlaneGeometry",
+            "width": self.width,
+            "height": self.height,
+            "widthSegments": self.widthSegments,
+            "heightSegments": self.heightSegments,
         }
